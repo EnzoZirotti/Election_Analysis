3984797fb79316857a0244d769e53dc7436591f4
Enzo <zirotti32@gmail.com>
2021-12-21 21:56:29 -0700

    New Files added

---
+++ PyPoll.py
@@ -0,0 +1,5 @@
+#1.Open the data file.
+#2.Write down the names of all the candidates.
+#3.Add a vote count for each candidate.
+#4.Get the total votes for each candidate.
+#5.Get the total votes cast for the election.
