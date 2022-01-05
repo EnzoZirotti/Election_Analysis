94ddbfe1ae5347f5adb4f90dc28166d123b152be
Enzo <Zirotti32@gmail.com>
2022-01-05 00:05:19 -0700

    Updates

--- PyPoll.py
+++ PyPoll.py
@@ -3,3 +3,13 @@
 #3.Add a vote count for each candidate.
 #4.Get the total votes for each candidate.
 #5.Get the total votes cast for the election.
+
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Election_Analysis","Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
